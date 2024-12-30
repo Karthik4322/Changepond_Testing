adc99f128fa7e1b31932f2e090257b3576163e92
karthik <shanmugamkarthik848@gmail.com>
2024-12-30 19:03:58 +0530

    crud operations,array string capitalize task added

---
+++ Task -7 Array String Capitalize/ArrayFunction.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayproj
+{
+     class ArrayFunction
+    {
+        
+        int Isearchelement;
+        int[] numbers = { 10, 200, 6, 4, 51 };
+        string[] fruits = { "Apple", "Banana", "Mango", "Orange", "Grapes" };
+        string Ssearchelement = "";
+        string firstvalue;
+        public void showArrayFunction()
+        {
+
+            int[] newNumbers = new int[5];
+
+            Console.WriteLine(Array.IndexOf(numbers, 6));
+            Console.WriteLine(Array.BinarySearch(numbers, 6));
+            numbers.CopyTo(newNumbers, 0);
+
+
+            foreach (int nn in newNumbers)
+                Console.WriteLine(nn);
+
+
+            Array.Resize(ref newNumbers, 7);
+            newNumbers[5] = 15;
+            newNumbers[6] = 13;
+
+
+            foreach (int nn in newNumbers)
+                Console.WriteLine(nn);
+
+            Array.Clear(newNumbers); // To delete all elements from that array
+            Array.Reverse(numbers);  // To reverse the numbers in the array
+            Console.WriteLine("===================================================");
+            foreach (int num in numbers)
+                Console.WriteLine(num);
+        }
+
+            public void FruitFound()
+            {
+                Console.WriteLine("Enter a fruit wanted to be searched: ");
+                string ssearchelement = Console.ReadLine();
+                ssearchelement = TitleCase(ssearchelement);
+
+                if (Array.Exists(fruits, element => element.Equals(ssearchelement, StringComparison.OrdinalIgnoreCase)))
+                {
+                    Console.WriteLine($"The {ssearchelement} element is present in the array.");
+                }
+                else
+                {
+                    Console.WriteLine($"The {ssearchelement} element is not present in the array.");
+                }
+            chooseoption();
+            }
+
+            public string TitleCase(string input)
+            {
+                if (string.IsNullOrEmpty(input))
+                {
+                    return input; // if there is empty space character
+                }
+
+                TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;// predefined function to convert string into Titlecase using TextInfo
+                return textInfo.ToTitleCase(input.ToLower());
+            }
+        
+            public void chooseoption()
+            {
+            Console.WriteLine("Enter the option to be choosed:\n 1.Display fruit 2. Find Fruit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    displayfruit();
+                    break;
+                case 2:
+                    FruitFound();
+                    break;
+                default:
+                    Console.WriteLine("You haven't selected the option");
+                    break;
+            }
+
+        }
+        public void displayfruit()
+        {
+            foreach (string arr in fruits) { 
+            Console.WriteLine(arr);
+        }
+            chooseoption();
+        }
+      
+
+    }
+}
---
+++ Task -8 List CRUD operations/ListFunctions.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Net.Quic;
+
+namespace Arrayproj
+{
+    class ListFunctions
+    {
+
+        List<string> fruits = new List<string>(){ "Apple", "Banana", "Mango", "Orange", "Grapes" };
+
+        string newItem;
+        public void ListFunc() {
+
+            List<int> numbers = new List<int>() { 2, 40, 10, 15, 100, 74 };
+            List<int> newnumbers = new List<int>() { 5, 2, 3 };
+
+            //numbers.Add(11);
+            numbers.Insert(2, 55);
+            //numbers.InsertRange(3, newnumbers);
+            //numbers.AddRange(newnumbers);
+            //numbers.Sort();
+            //numbers.Reverse();
+            //numbers.Remove(40);
+            numbers.RemoveAt(2);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+
+
+        public void chooseoption()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the option to be choosed:\n1. Display fruit \n2. Add Fruit \n3. Update Fruit \n4. Delete Fruit \n5. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (option)
+                {
+                    case 1:
+                        displayfruit();
+                        break;
+                    case 2:
+                        addfruit();
+                        break;
+                    case 3:
+                        updatefruit();
+                        break;
+                    case 4:
+                        deletefruit();
+                        break;
+                    case 5:
+                        goto quit;
+                    default:
+                        Console.WriteLine("You haven't selected the given option");
+                        break;
+                }
+            }
+                    quit:;
+        }
+
+        public void displayfruit()
+        {
+            foreach (string arr in fruits)
+            {
+                Console.WriteLine(arr);
+            }
+
+        }
+
+        public void addfruit()
+        {
+            Console.Write("Enter a fruit name to add : ");
+            newItem = Console.ReadLine();
+            ComparewithExisting();
+
+        }
+
+        public void updatefruit()
+        {
+            foreach (string s in fruits) Console.WriteLine(s);
+
+            Console.WriteLine("Enter the element wanted to be get updated from list");
+            string replacingfrom = Console.ReadLine();
+
+
+            for (int indexvalue = 0; indexvalue < fruits.Count; indexvalue++)
+            {
+                if (string.Compare(fruits[indexvalue], replacingfrom, StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    Console.WriteLine($"The fruit {replacingfrom} is found at {indexvalue}");
+                    ElementFound(indexvalue);
+                    break;  
+                }
+            }
+        }
+        public int ElementFound(int elementIndex)
+        {
+
+            Console.Write("Enter the updating fruit name : ");
+            string replaceWith = Console.ReadLine();
+
+            for (int indexvalue = 0; indexvalue < fruits.Count; indexvalue++)
+            {
+                if (fruits[indexvalue].Equals(replaceWith,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("The element is already exists not necessary to update with same value");
+                    break;
+                }
+                else
+                {
+                    fruits[elementIndex] = replaceWith;
+                    Console.WriteLine("After the changes made on list :");
+                    foreach (string s in fruits) Console.WriteLine(s);
+                    break;
+                }
+               
+            }
+
+            return 0;
+        }
+
+        public void deletefruit()
+        {
+
+            foreach (string s in fruits) Console.WriteLine(s);
+
+            Console.WriteLine("Enter a element to be get deleted");
+            string deletingfrom = Console.ReadLine();
+            bool isAvailable = false;
+           
+                for (int indexvalue = 0; indexvalue < fruits.Count; indexvalue++)
+                {
+                    if (string.Compare(fruits[indexvalue], deletingfrom, StringComparison.OrdinalIgnoreCase) == 0)
+                    {
+                        // Console.WriteLine($"The fruit {deletingfrom} is found at {indexvalue}");
+                        // DeleteElementFound(indexvalue);
+                        fruits.RemoveAt(indexvalue);
+                        Console.WriteLine($"The fruit {deletingfrom} is deleted ");
+                        Console.WriteLine("After the changes made on list :");
+                        foreach (string s in fruits) Console.WriteLine(s);
+                    isAvailable = true;
+                    }
+
+                }
+            
+            if(isAvailable==false)
+            {
+                Console.WriteLine($"The {deletingfrom} item is not present in list to delete");
+            }
+        }
+
+        public void ComparewithExisting() { 
+
+            for(int indexvalue = 0; indexvalue < fruits.Count; indexvalue++) {
+             
+            if (fruits[indexvalue].Equals(newItem,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"The fruit {newItem} is found at {indexvalue}");
+                    Console.WriteLine($"You can't add this {newItem} again");
+                    break;
+                }
+                else
+                {
+                    fruits.Add(newItem);
+                    Console.WriteLine($"The {newItem} added successfully ");
+                    Console.WriteLine("After the changes made on list : ");
+                    displayfruit();
+                    break;
+                }
+
+            }
+        }
+
+
+}
+    }
+
+
+
